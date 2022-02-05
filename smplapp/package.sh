65631cf3d61fe7f29be6c87b25c74e84a799c697
Your Name <you@example.com>
2022-02-06 08:34:47 +1300

    simpleserver packaging fix

--- smplapp/package.sh
+++ smplapp/package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # tar -cvzf app.tar.gz app/
 apphash=smpl`git rev-parse --short HEAD`
-zip -r ${apphash}.zip app
+zip -r ${apphash}.zip *.j*
 docker build -t taroball/taro1:${apphash} .
 docker push taroball/taro1:${apphash}
