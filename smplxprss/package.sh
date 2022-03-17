4c2f3a9c2961f977b80fd2010ad8cf739ba7d1a7
Your Name <you@example.com>
2022-03-17 16:59:25 +1300

    pack modules as well

--- smplxprss/package.sh
+++ smplxprss/package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # tar -cvzf app.tar.gz app/
 apphash=xpss`git rev-parse --short HEAD`
-zip -r ${apphash}.zip *.j*
+zip -r ${apphash}.zip *.j* node_modules/*
 docker build -t taroball/taro1:${apphash} .
 docker push taroball/taro1:${apphash}
